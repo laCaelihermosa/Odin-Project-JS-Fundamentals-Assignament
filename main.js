f115d6e166334d4b35054c81e8dc9915916e1643
laCaelihermosa <fabian_lr0505@outlook.com>
2023-03-28 14:39:43 -0600

    Commit, adding fyles

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+//1 Declare two variables: admin and name.
+let admin;
+let name;
+
+//Assign the value "John" to name.
+name = "john";
+
+//Copy the value from name to admin.
+admin = name;
+
+alert(admin);
+
+//alert pops up with no '' quotes inside the brackets
+//when alert('admin'); ()or even double quotation alert = admin
+
+// ('')__/ 
+
+//2 Create a variable with the name of our planet. How would you name such a variable?
+//Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+let ourPlanetName = heart;
+
+let currentName = "yournamehere";
+
+//3 considering :
+const BIRTHDAY = '18.04.1982'; // make birthday uppercase?
+
+const AGE = someCode(BIRTHDAY); // make age uppercase?
+
+//We generally use upper case for constants that are “hard-coded”. Or, in other words, when the value is known prior to execution and directly written into the code.
+
+//In this code, birthday is exactly like that. So we could use the upper case.
+
+//In contrast, age is evaluated in run-time. Today we have one age, a year after we’ll have another one. It is constant in a sense that it does not change through the code execution. But it is a bit “less of a constant” than birthday: it is calculated, so we should keep the lower case for it.
+
+
+
+
+//ODINS COMPLEMENTARY EXERCISES
